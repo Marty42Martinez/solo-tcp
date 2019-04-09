d35b16b8255f69a8d865dda8ce6a8522f65f30a2
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-09 09:38:07 -0700

    writing streams by chunks

---
+++ writeStream.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const ws = fs.createWriteStream('./write.json', {flags: 'a'});
+
+ws.write('{ "name": ');
+ws.write('fido, ');
+ws.write('"speci');
+ws.write('es": dog }');
+ws.end();
